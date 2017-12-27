8f43eca182d39a0aa3a4e0babe8e895aeb526759
Markus_N <31623011+MarkusN-fr@users.noreply.github.com>
2017-12-27 13:53:59 +0100

    Deuxieme morceau : debut de modif des modalités
    Un début d'adaptation des modalités pourqu'elles soient conformes à celles utilisées l'année dernière

--- Mise_en_forme.R
+++ Mise_en_forme.R
@@ -9,10 +9,13 @@ load("Travail_MrMargerie/Workspace 16 04.RData")
 # On vire les variables non utilisées par la promo précédente
 # Seul problème : eux ils ont des données qu'on n'a pas ...
 
+id.numbers <- rownames(dat)
 dat <- as.tibble(dat)
 
 dat2 <- dat %>%
-  select(TYPE = type,
+  add_column(ID = id.numbers) %>%
+  select(ID = ID,
+         TYPE = type,
          FORME = forme,
          TAIL_LONG = Longueur,
          TAIL_LARG = largeur,
@@ -45,11 +48,36 @@ dat2 <- dat %>%
          #POT_ECO = ?
          Fond = nature_fond)
 
+
 # On fait alors en sorte d'avoir les mêmes modalités de variables partout :
 
 dat3 <- dat2 %>%
-  transmute(
-    
+  mutate(
+    TYPE = fct_recode(TYPE,
+                      "BASSIN" = "bassin",
+                      "MARE" = "mare"),
+    FORME = fct_recode(FORME,
+                      "carre/rectangle" = "carre_rectangle",
+                      "carre/rectangle" = "triangle",
+                      "complexe" = "complexe",
+                      "complexe" = "patatoide",
+                      "ronde/ovale" = "ronde_ovale"),
+    CONTEXTE = fct_recode(CONTEXTE,
+                          "annexes_routieres" = "annexe_routiere_ferroviere",
+                          "bois_feuillus" = "bois_de_feuillus",
+                          "bois_feuillus" = "fourres_bosquets",
+                          "culture" = "culture",
+                          "parmi_habitations/milieux_urbains" = "jardin_parc_cour_de_ferme",
+                          "prairie_mesophile" = "prairie_humide",
+                          "prairie_mesophile" = "lande_humide",
+                          "tourbière" = "bas_marais_tourbiere_alcaline",
+                          "je_ne_sais_pas" = "indetermine"),
+    TOPO = fct_recode(TOPO,
+                      "fond_vallee" = "fond_de_vallee"),
+    LIAI_HYDRO = fct_recode(LIAI_HYDRO,
+                            "ecoulement_a_sec_en_sortie" = ,
+                            "ecoulement_actif_en_sortie" = ,
+                            "jamais_decoulement_en_sortie" = ),
   )
 
 
